6b047c4a6967ff7d897d22d1a824d0f541c6ca38
Oriery <83728532+Oriery@users.noreply.github.com>
2022-09-19 17:50:07 +0300

    refactor

--- srv/main.js
+++ srv/main.js
@@ -2,14 +2,16 @@ module.exports = cds.service.impl(async function () {
     const { Users } = cds.entities
 
     this.on("getUsersByIds", async (req) => {
-        const {ids} = req.data
+        const { ids } = req.data
         console.log(ids)
 
         if (ids) {
-            var response = await cds.tx(req).run(SELECT.from(Users).where `ID in ${ids}`)
+            var response = await cds
+                .tx(req)
+                .run(SELECT.from(Users).where`ID in ${ids}`)
             return response
         } else {
-            req.reject(400, 'No "ids" array found in body object');
+            req.reject(400, 'No "ids" array found in body object')
         }
     })
 })
